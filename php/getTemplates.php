581badf3033dd893eb5db0c56f38b7d8a028f4c5
NORRIS HIPOLITO <norrishipolito@gmail.com>
2021-06-22 05:42:33 +0800

    deleting comments

--- php/getTemplates.php
+++ php/getTemplates.php
@@ -1,13 +1,4 @@
 <?php 
-    // $host="localhost";
-    // $user="root";
-    // $pass="";
-    // $data="waitlist";
-    // $db = mysqli_connect($host,$user,$pass,$data);
-    //     //local
-    // if (!$db) {
-    //     die("Connection failed: " . mysqli_connect_error());
-    // }
     require 'conn.php';
     $tempName =$_GET['tempName'];
     $query ="SELECT * FROM emailtemplates WHERE TemplateName = '$tempName'";
--- php/search.php
+++ php/search.php
@@ -1,14 +1,5 @@
 <?php 
     include '../php/functions.php';
-    // $host="localhost";
-    // $user="root";
-    // $pass="";
-    // $data="waitlist";
-    // $db = mysqli_connect($host,$user,$pass,$data);
-    //     //local
-    // if (!$db) {
-    //     die("Connection failed: " . mysqli_connect_error());
-    // }
     require 'conn.php';
     $name=$_GET['name'];
     $type=$_GET['type'];
