fbcc3229e3eae9b659104b733b70e0016bf557eb
norrisasd <61019363+norrisasd@users.noreply.github.com>
2021-06-23 23:44:39 +0800

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -4,15 +4,15 @@
 //     $pass="";
 //     $data="waitlist";
 //heroku
-//     $host="remotemysql.com";
-//     $user="GuUtM46wKG";
-//     $pass="a6wDOZNQrQ";
-//     $data="GuUtM46wKG";
+    $host="remotemysql.com";
+    $user="GuUtM46wKG";
+    $pass="a6wDOZNQrQ";
+    $data="GuUtM46wKG";
 //siteground
-    $host="localhost";
-    $user="uarg4jgrrxrfe";
-    $pass="e#3$2316@fF[";
-    $data="dbwdgw3oahbswe";
+//     $host="localhost";
+//     $user="uarg4jgrrxrfe";
+//     $pass="e#3$2316@fF[";
+//     $data="dbwdgw3oahbswe";
     $db = mysqli_connect($host,$user,$pass,$data);
         //local
     if (!$db) {
